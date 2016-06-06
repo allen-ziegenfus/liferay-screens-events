5ef49ceb0b0771696dbb298323fc43ae4ad3a143
allen-ziegenfus <allen.ziegenfus@liferay.com>
2016-06-06 18:16:44 +0200

    get login working

---
+++ liferay-screens-events/liferay-screens-events/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  liferay-screens-events
+//
+//  Created by Allen Ziegenfus on 06/06/16.
+//  Copyright © 2016 Allen Ziegenfus. All rights reserved.
+//
+
+import UIKit
+import LiferayScreens
+
+class ViewController: UIViewController, LoginScreenletDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.login.delegate = self;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+     func screenlet(screenlet: BaseScreenlet,
+                    onLoginResponseUserAttributes attributes: [String:AnyObject]) {
+        print("login done \(attributes)")
+        
+    }
+    
+     func screenlet(screenlet: BaseScreenlet,
+                    onLoginError error: NSError) {
+        print("login error \(error)")
+        
+    }
+
+    @IBOutlet weak var login: LoginScreenlet!
+}
+
