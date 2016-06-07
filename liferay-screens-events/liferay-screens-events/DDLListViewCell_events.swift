976cfae14651d5a448b2c03c21a024099cb08642
allen-ziegenfus <allen.ziegenfus@liferay.com>
2016-06-07 17:21:27 +0200

    get subclass working

---
+++ liferay-screens-events/liferay-screens-events/DDLListViewCell_events.swift
@@ -0,0 +1,89 @@
+//
+//  DDLListViewCell_events.swift
+//  liferay-screens-events
+//
+//  Created by Allen Ziegenfus on 07/06/16.
+//  Copyright © 2016 Allen Ziegenfus. All rights reserved.
+//
+
+import UIKit
+import LiferayScreens
+
+
+
+public class DDLListViewCell_events: UITableViewCell {
+    
+   
+    @IBOutlet weak var imgBack: UIImageView!
+
+    
+    @IBOutlet weak var location: UILabel!
+    
+    @IBOutlet weak var title: UILabel!
+ 
+
+    
+    let imageParallaxFactor: CGFloat = 20
+    
+    var imgBackTopInitial: CGFloat!
+    var imgBackBottomInitial: CGFloat!
+    
+    private let dateFormatter = NSDateFormatter()
+    
+    public var record: DDLRecord? {
+        didSet {
+            if let titleField = record?.fieldBy(name: "title") {
+                title.text = titleField.currentValueAsString
+            }
+            
+            if let locationField = record?.fieldBy(name: "location_label") {
+                location.text = locationField.currentValueAsString
+            }
+            setMyImage(record)
+        }
+    }
+    
+    func setMyImage(record: DDLRecord?) {
+        if let field = record?["logo"] {
+            
+            if let currentValue = field.currentValueAsLabel {
+                
+                if currentValue != "" {
+                    
+                    guard let data = currentValue.dataUsingEncoding(NSUTF8StringEncoding) else {
+                        return
+                    }
+                    
+                    guard let serialized = try? NSJSONSerialization.JSONObjectWithData(data, options: []) else {
+                        return
+                        
+                    }
+                    //currentValue	String	"{\"groupId\":\"20182\",\"uuid\":\"504f6a21-de72-46a7-bcbf-e9038e2b9482\",\"version\":\"1.0\"}"
+                    
+                    guard let jsonObject = serialized as? JSONObject else {
+                        return
+                    }
+                    //object.groupId
+                    
+                    //object.version
+                    
+                    let group = (jsonObject["groupId"] as AnyObject? as? String) ?? ""
+                    let uuid = (jsonObject["uuid"] as AnyObject? as? String) ?? ""
+                    let version = (jsonObject["version"] as AnyObject? as? String) ?? ""
+                    
+                    let url = "https://mdata.liferay.com/documents/" + group + "/" + uuid
+                    //let imageName = "lpsf-2015-default.jpg"
+                    let nsurl = NSURL(string: url)
+                    let imagedata = NSData(contentsOfURL: nsurl!)
+                    let image = UIImage(data:imagedata!)
+                    imgBack.image = image
+                    
+                    
+                }
+                
+            }
+
+        }
+    }
+
+}
--- liferay-screens-events/liferay-screens-events/DDLListView_events.swift
+++ liferay-screens-events/liferay-screens-events/DDLListView_events.swift
@@ -18,147 +18,29 @@ public class DDLListView_events: BaseListTableView, DDLListViewModel {
     public var labelFields: [String] = []
     
     
+    override public func doRegisterCellNib(id id: String) {
+        let nib = UINib(
+            nibName: "DDLListViewCell_events",
+            bundle: NSBundle(forClass: self.dynamicType))
+        
+        self.tableView!.registerNib(nib, forCellReuseIdentifier: id)
+    }
+    
     override public func doFillLoadedCell(row row: Int, cell: UITableViewCell, object:AnyObject) {
-        
-        if let record = object as? DDLRecord {
+        if let record = object as? DDLRecord,
+            issueCell = cell as? DDLListViewCell_events {
             
-            cell.textLabel?.text = "Event: " + composeLabel(record)
-            cell.detailTextLabel?.text = "Test Detail Label"
+            issueCell.record = record
             
-           
-            ///documents/67510365/8c4a085d-5548-45f8-bb8c-5aae4cecab4c?
-            
-            //let imageName = "lpsf-2015-default.jpg"
-            //let image = UIImage(named: imageName)
-            //cell.imageView!.image = image
-            
-            
-            if let field = record["logo"] {
-                
-                if let currentValue = field.currentValueAsLabel {
-                    
-                    if currentValue != "" {
-                        
-                        guard let data = currentValue.dataUsingEncoding(NSUTF8StringEncoding) else {
-                            return
-                        }
-                        
-                        guard let serialized = try? NSJSONSerialization.JSONObjectWithData(data, options: []) else {
-                        return
-                            
-                        }
-                        //currentValue	String	"{\"groupId\":\"20182\",\"uuid\":\"504f6a21-de72-46a7-bcbf-e9038e2b9482\",\"version\":\"1.0\"}"
-                        
-                        guard let jsonObject = serialized as? JSONObject else {
-                            return
-                        }
-                            //object.groupId
-                        
-                        //object.version
-                        
-                        let group = (jsonObject["groupId"] as AnyObject? as? String) ?? ""
-                        let uuid = (jsonObject["uuid"] as AnyObject? as? String) ?? ""
-                        let version = (jsonObject["version"] as AnyObject? as? String) ?? ""
-                        
-                        let url = "https://mdata.liferay.com/documents/" + group + "/" + uuid
-                        //let imageName = "lpsf-2015-default.jpg"
-                        let nsurl = NSURL(string: url)
-                        let imagedata = NSData(contentsOfURL: nsurl!)
-                        let image = UIImage(data:imagedata!)
+        }
+    }
+    
 
-                        
-                        cell.imageView!.image = image
-                       
-                        
-                    }
-                    
-                }
-                
-            }
 
-     
-            
-            
-            cell.accessoryType = .DisclosureIndicator
-            
-            cell.accessoryView = nil
-            
-        }
-        
-    }
-    
-    
-    override public func doFillInProgressCell(row row: Int, cell: UITableViewCell) {
-        
-        cell.textLabel?.text = "..."
-        
-        cell.accessoryType = .None
         
         
         
-        if let image = NSBundle.imageInBundles(
-            
-            name: "default-hourglass",
-            
-            currentClass: self.dynamicType) {
-            
-            cell.accessoryView = UIImageView(image: image)
-            
-            cell.accessoryView!.frame = CGRectMake(0, 0, image.size.width, image.size.height)
-            
-        }
-        
-    }
-    
-  
-    
-    public func composeLabel(record: DDLRecord) -> String {
-        
-        var result: String = ""
-        
-        
-        
-        for labelField in labelFields {
-            
-            if let field = record[labelField] {
-                
-                if let currentValue = field.currentValueAsLabel {
-                    
-                    if currentValue != "" {
-                        
-                        result += currentValue
-                        
-                        result += " "
-                        
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        
-        
-        
-        if result.endIndex == result.startIndex {
-            
-            print("[ERROR] Can't compose the label for record. It seems the fields specified are not valid\n")
-            
-        }
-            
-        else {
-            
-            result.removeAtIndex(result.endIndex.predecessor())
-            
-        }
-        
-        
-        
-        return result
-        
-    }
-    
-    
+
     
     
     
